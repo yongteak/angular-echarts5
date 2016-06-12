a012d663615d123437c66c55b41289d9400ba5e7
尹现伟 <ios@yinxianwei.cn>
2016-06-12 22:08:57 +0800

    数据源切换bug

--- src/app/app.js
+++ src/app/app.js
@@ -4,26 +4,75 @@
 var app = angular.module('demoApp', ['angular-echarts3']);
 
 app.controller('MainCtrl', ['$scope', function($scope) {
-    $scope.aaa = "2323";
+    var vm = this;
+    var type = false;
+    vm.option = loadDataWithType(type);
+    
+    $scope.change = function() {
+        type = !type;
+        vm.option = loadDataWithType(type);
+    }
 
-        var vm = this;
-	// 指定图表的配置项和数据
-	vm.option = {
-	    title: {
-	        text: '测试'
-	    },
-	    tooltip: {},
-	    legend: {
-	        data: ['销量']
-	    },
-	    xAxis: {
-	        data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
-	    },
-	    yAxis: {},
-	    series: [{
-	        name: '销量2',
-	        type: 'bar',
-	        data: [5, 20, 36, 10, 10, 20]
-	    }]
-	};
+    function loadDataWithType(type) {
+        if (type) {
+            return {
+                title: {
+                    text: '测试'
+                },
+                tooltip: {},
+                legend: {
+                    data: ['销量']
+                },
+                xAxis: {
+                    data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+                },
+                yAxis: {},
+                series: [{
+                    name: '销量2',
+                    type: 'bar',
+                    data: [5, 20, 36, 10, 10, 20]
+                }]
+            };
+        }
+        return {
+            title: {
+                text: '某站点用户访问来源',
+                subtext: '纯属虚构',
+                x: 'center'
+            },
+
+            tooltip: {
+                trigger: 'item',
+                formatter: "{a} <br/>{b} : {c} ({d}%)"
+            },
+            legend: {
+                show: false,
+                orient: 'vertical',
+                left: 'left',
+                data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
+            },
+            series: [{
+                name: '访问来源',
+                type: 'pie',
+                radius: '55%',
+                center: ['50%', '60%'],
+                data: [
+                    { value: 335, name: '直接访问' },
+                    { value: 310, name: '邮件营销' },
+                    { value: 234, name: '联盟广告' },
+                    { value: 135, name: '视频广告' },
+                    { value: 1548, name: '搜索引擎' }
+                ],
+                itemStyle: {
+                    emphasis: {
+                        shadowBlur: 10,
+                        shadowOffsetX: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                }
+            }]
+        };
+    }
+
+
 }]);
